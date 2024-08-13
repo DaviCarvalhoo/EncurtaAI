3715bdea46c989c765644ee75f68b74c00636896
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-08-13 17:39:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Encurtador de Links 
+## Um encurtador de links feito com Spring.
+No projeto fiz o uso de: 
+* JPA
+* PostgreSQL  
+* Railway
+
---
+++ src/test/java/dev/davi/EncurtaAI/EncurtaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davi.EncurtaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
